80dd540c1bc83611ced6108a49ff5f427d862b54
divakararao76 <84025602+divakararao76@users.noreply.github.com>
2021-05-14 21:13:40 +0530

    Update primefacors.cpp

--- primefacors.cpp
+++ primefacors.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 long long int N=1000001;
 long long int seive[1000001];
@@ -37,4 +37,59 @@ int main() {
 	    }
 	}
 	return 0;
+}
+*/
+#include <bits/stdc++.h>
+#include<cmath>
+using namespace std;
+
+ void pFactors(int n)
+{
+    
+    int c=0;
+    
+    while (n % 2 == 0)
+    {
+        c++;
+        n = n/2;
+    
+
+    }
+    cout<<"2"<<"^"<<c;
+        
+    for (int i = 3; i <= sqrt(n); i = i + 2)
+    {
+        c=0;
+        
+        while (n % i == 0)
+        {
+                c++;
+                n = n/i;
+         }
+        if(c!=0)
+        cout<<"*"<<i<<"^"<<c;
+    }
+   
+    if (n > 2)
+    cout<<"*"<<n<<"^"<<"1";
+      
+}
+ 
+int main()
+{
+    int n,t;
+    cin>>t;
+    if(t>=1&&t<=1000000)
+        while(t--)
+        {
+            cin>>n;
+                if (n>=1 && n<=1000000)
+                {
+                    pFactors(n);
+                }
+                cout<<"\n";
+     
+    }
+    
+    return 0;
 }
