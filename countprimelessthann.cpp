d5e70f53b9b1e2a725d9930c2ff3475d0e25ecc4
divakararao76 <84025602+divakararao76@users.noreply.github.com>
2021-05-14 07:34:59 +0530

    Update countprimelessthann.cpp

--- countprimelessthann.cpp
+++ countprimelessthann.cpp
@@ -52,6 +52,40 @@ int main()
 }
 */
  
-			   
+long long int x=1000000;
+long long int s[1000000];
+void genseiver()
+{
+	for(int i=0;i<x;i++)  //s[]={1,1,1,1,...1000000 times}
+	s[i]=1;
+	s[0]=s[1]=0;		//s[]={0,0,1,1,1....}
+	for(int i=2;i*i<x;i++)
+	{
+		if(s[i]==1)				//i=2 then s[]={0,0,1,1,0,1,0,1,0,1,0,1,0,...}
+		{					//i=3 then s[]={0,0,1,1,0,1,0,1,0,0,0,1,0,1,0,0,...}
+			for(int j=i*i;j<=x;j=j+i)
+				s[j]=0;
+		}
+	}
+}
+int primecount(int num)
+{
+	int count=0,loopi;
+	for(loopi=2;loopi<=n;loopi++)
+	{
+		if(s[loopi]==1)
+		count++;
+	}
+	return count;
+}
+
+int main()
+{
+	genseiver();		//call to genseiver() function
+	int number;
+	cin>>number;
+	cout<<primecount(numnber);
+	return 0;
+}
 		   
 		   
