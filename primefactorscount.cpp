4d693fac3efe40fd88bab1678348be2ed470e8b2
divakararao76 <84025602+divakararao76@users.noreply.github.com>
2021-05-14 19:39:46 +0530

    Update primefactorscount.cpp

--- primefactorscount.cpp
+++ primefactorscount.cpp
@@ -1,45 +1,38 @@
 #include <iostream>
 using namespace std;
 long long int N=1000001;
-long long int seive[1000001];
-void gen_modifiedsieve()
+long long int s[1000001];
+void gensieve()
 {
-    seive[0]=seive[1]=1;
-    for(int i=2;i<=N;i++)seive[i]=i;
-    for(int i=2;i*i<=N;i++)
+    for(int i=0;i<N;i++)    // s[]={1,1,1,1,1,1.......1000000 times}
+    s[i]=1;
+    s[0]=0;
+    s[1]=0;		//s[]={0,0,1,1,1,1,1,1,1,....,1000000 times}
+    for(long int i=2;i*i<N;i++)
     {
-        if(seive[i]==i)
-        {
-            for(int j=i*i;j<=N;j+=i)
-            {
-                if(seive[j]==j)
-                {
-                    seive[j]=i;
-                }
-            }
-        }
-    }
-    
-}
-
-int main() {
-	gen_modifiedsieve();
-	 int q;
-	cin>>q;
-	while(q--)
-	{
-	     long int n,c=0;
-	    cin>>n;
-	    /*while(n!=1)
+	    if(s[i]==1)
 	    {
-	        cout<<seive[n]<<" ";
-	        n=n/seive[n];
-	    }*/
-		for(long long int i=2;i<N;i++)
-		if(n==seive[i])
-		c++;
-
-		cout<<c<<endl;
+		    for(int j=i*i;j<=N;j=j+i)
+		    {
+			    if(s[j]==1)
+				s[i]=s[i]+1;	// where i=2 s[]={0,0,500000,1,0,1,0,1,0,1,0....}
+		     	    s[j]=0;
+		    }
+	    }
+    }
+}
+int main()
+{
+	gensieve(); 		//call genseive() funtion
+     	long long int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		cout<<s[n]<<endl;
 	}
 	return 0;
 }
+	
+   
