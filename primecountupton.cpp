175533b066e263197bdc68ceb70ce45610682d41
divakararao76 <84025602+divakararao76@users.noreply.github.com>
2021-05-14 07:26:00 +0530

    Update primecountupton.cpp

--- primecountupton.cpp
+++ primecountupton.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int main()
+/*int main()
 {
     long int number,loopi,count1,count2,loopj;
     cin>>number;
@@ -16,4 +16,35 @@ int main()
     }
 	cout<<count2;
     return 0;
+}
+*/
+
+long long int x=1000000;
+long long int s[1000000];
+void genseiver()
+{
+	for(int i=0;i<x;i++)  //s[]={1,1,1,1,...1000000 times}
+	s[i]=1;
+	s[0]=s[1]=0;		//s[]={0,0,1,1,1....}
+	for(int i=2;i*i<x;i++)
+	{
+		if(s[i]==1)				//i=2 then s[]={0,0,1,1,0,1,0,1,0,1,0,1,0,...}
+		{					//i=3 then s[]={0,0,1,1,0,1,0,1,0,0,0,1,0,1,0,0,...}
+			for(int j=i*i;j<=x;j=j+i)
+				s[j]=0;
+		}
+	}
+}
+int main()
+{
+	genseiver();		//call to genseiver() function
+	int number,loopi,count=0;
+	cin>>number;
+	for(loopi=2;loopi<=n;loopi++)
+	{
+		if(s[loopi]==1)
+		count++;
+	}
+	cout<<count;
+	return 0;
 }
